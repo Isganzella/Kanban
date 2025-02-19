77006e311416d525ac5a046d8ffaa240d73984a3
Marlos Isganzella <misganzella@gmail.com>
2025-02-18 22:52:01 -0300

    Ajuste no Board

--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Board = () => {
   return (
     <main className='pt-8 pr-40 pl-40 bg-[#F2F4F7] h-screen'>
-        boards
+        <div className='container mx-auto flex justify-between items-center'>
+            <h1 className='text-2xl font-bold text-[#1d2939]'>Board</h1>
+            <button className='px-[20px] py-4 bg-green-500 rounded-md text-white font-bold'>Adicionar tarefa</button>
+        </div>
     </main>
   )
 }
